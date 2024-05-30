c96282c88f0bada2fd2e342b4f0c064ef53df2aa
PWR <pwrpntr@gmail.com>
2024-05-30 18:01:34 +0200

    add missed files

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import deskStructure from './deskStructure'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,27 @@ export default defineConfig({
   projectId: '5pk6dy85',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({ structure: deskStructure }),
+    visionTool()
+  ],
+
+  document: {
+    newDocumentOptions: (prev, { creationContext }) => {
+      const { type } = creationContext;
+      const DISABLED_TYPES = [
+        'about',
+        'contributionsToResearch',
+        'cvAndContact',
+        'textAndArchive',
+        'mainPageTop'
+      ];
+      if (type === 'global') {
+        return prev.filter((template) => !DISABLED_TYPES.includes(template.templateId));
+      }
+      return prev;
+    },
+  },
 
   schema: {
     types: schemaTypes,
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import ContentEditor from './ContentEditor'
+import About from './About'
+import CVAndContact from './CVAndContact'
+import ContributionsToResearch from './ContributionsToResearch'
+import TextAndArchive from './TextAndArchive'
+import MainPageTop from './MainPageTop'
+import Work from './Work'
+import Documentation from './Documentation'
+
+export const schemaTypes = [
+    ContentEditor,
+    About,
+    CVAndContact,
+    TextAndArchive,
+    ContributionsToResearch,
+    MainPageTop,
+    Work,
+    Documentation
+]
